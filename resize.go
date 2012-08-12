6248bef5aab5abb1cb730b6c08f18eed5c8b5e08
George Bashi <jamil@georgebashi.com>
2012-08-12 20:54:07 +0100

    switch to graphicsmagick

--- resize.go
+++ resize.go
@@ -1,6 +1,6 @@
 package main
 
-// #cgo pkg-config: MagickWand
+// #cgo pkg-config: GraphicsMagickWand
 // #include <wand/magick_wand.h>
 import "C"
 
@@ -8,19 +8,10 @@ import (
 	"unsafe"
 )
 
-var free_wands = make(chan *C.MagickWand, 10)
-
-func init() {
-	C.MagickWandGenesis()
-}
-
 func resize(file string, width int, height int) []byte {
-	var wand *C.MagickWand
-	select {
-		case wand = <-free_wands:
-		default:
-			wand = C.NewMagickWand()
-	}
+	wand := C.NewMagickWand()
+	defer C.DestroyMagickWand(wand)
+
 	C.MagickReadImage(wand, C.CString(file))
 
 	cur_width := float64(C.MagickGetImageWidth(wand))
@@ -42,22 +33,14 @@ func resize(file string, width int, height int) []byte {
 
 	C.MagickStripImage(wand)
 	if r_width > 5 && r_height > 5 {
-		C.MagickSampleImage(wand, (C.size_t)(dest_width * 5), (C.size_t)(dest_height * 5))
+		C.MagickSampleImage(wand, (C.ulong)(dest_width * 5), (C.ulong)(dest_height * 5))
 	}
-	C.MagickResizeImage(wand, (C.size_t)(dest_width), (C.size_t)(dest_height), C.LanczosFilter, 1)
-	C.MagickCropImage(wand, (C.size_t)(width), (C.size_t)(height), (C.ssize_t)(crop_x), (C.ssize_t)(crop_y))
-	C.MagickSetImageCompressionQuality(wand, 65)
+	C.MagickResizeImage(wand, (C.ulong)(dest_width), (C.ulong)(dest_height), C.LanczosFilter, 1)
+	C.MagickCropImage(wand, (C.ulong)(width), (C.ulong)(height), (C.long)(crop_x), (C.long)(crop_y))
 
 	size := 0
-	buf := C.MagickGetImageBlob(wand, (*C.size_t)(unsafe.Pointer(&size)))
+	buf := C.MagickWriteImageBlob(wand, (*C.size_t)(unsafe.Pointer(&size)))
 	defer C.MagickRelinquishMemory(unsafe.Pointer(buf))
-
-	C.ClearMagickWand(wand)
-	select {
-		case free_wands <- wand:
-		default:
-			C.DestroyMagickWand(wand)
-	}
 
 	return C.GoBytes(unsafe.Pointer(buf), C.int(size))
 }
